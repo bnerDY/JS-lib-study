62b2e7b96577a85d2fc47e794c5f49a2cab086db
DINGXIN YU <dingxin.yu@uqconnect.edu.au>
2016-09-20 17:55:41 +0800

    no message

--- redux-instagram/learn-redux/client/components/Main.js
+++ redux-instagram/learn-redux/client/components/Main.js
@@ -12,8 +12,7 @@ const Main = React.createClass({
                 <h1>
                     <Link to="/"> Reduxstagram </Link>
                 </h1>
-                {this.props.children}
-                
+                {React.cloneElement(this.props.children, this.props)}
             </div>
         )
     }
